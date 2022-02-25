90d5b994d1947ff42b725b4e46606819d2499e91
ealpago <alpagoemre@icloud.com>
2022-02-25 15:39:42 +0300

    tableView with segmentControl

--- UIKit Example/UIKit Example/New Group/UIKitComponentViewController.swift
+++ UIKit Example/UIKit Example/New Group/UIKitComponentViewController.swift
@@ -8,25 +8,41 @@
 import UIKit
 
 class UIKitComponentViewController: UIViewController {
-
-    @IBOutlet var componentTableView: UITableView!
-
+    @IBOutlet var nbaSegmented: UISegmentedControl!
+    @IBOutlet var nbaTableView: UITableView!
+    
+    let easternConference = ["Bulls", "Heat", "76ers", "Bucks", "Cavaliers", "Celtics", "Raptors", "Nets", "Hornets", "Hawks", "Wizard", "Knicks", "Pacers", "Pistons", "Magic"]
+    let westernConference = ["Suns", "Warriors", "Grizzlies", "Jazz", "Maverciks", "Nuggets", "Timberwolves", "Clippers", "Lakers", "Trail Blazers", "Spurs", "Pelicans", "Kings", "Thunder", "Rockets"]
+    
+    lazy var tableViewArray = easternConference
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        componentTableView.delegate = self
-//        componentTableView.dataSource = self
-        // Do any additional setup after loading the view.
+        nbaTableView.delegate = self
+        nbaTableView.dataSource = self
+        nbaSegmented.backgroundColor = .white
+    }
+    
+    @IBAction func changeSegment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            tableViewArray = easternConference
+            
+        }
+        if sender.selectedSegmentIndex == 1 {
+            tableViewArray = westernConference
+        }
+        nbaTableView.reloadData()
     }
 }
 
-//extension UIKitComponentViewController: UITableViewDelegate, UITableViewDataSource{
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    }
-//
-//
-//}
+extension UIKitComponentViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = tableViewArray[indexPath.row]
+        return cell
+    }
+}
